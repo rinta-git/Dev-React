101f3ce555cae2711825a359cedf15b45c4ca34a
unknown <rintaroyofficial@gmail.com>
2024-01-15 11:54:17 +0530

    resolved build failure

--- netflix-gpt/src/components/Header.js
+++ netflix-gpt/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { onAuthStateChanged } from "firebase/auth";
 import { addUser, removeUser } from "../utils/userSlice";
